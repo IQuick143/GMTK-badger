140bf134e5a377c43e2bdfb418c47ed8365ef487
bvasilchik <bvasilchik@webassign.net>
2019-08-12 17:13:14 -0400

    adding top percentages

--- gameSearcher.js
+++ gameSearcher.js
@@ -19,12 +19,14 @@ function doesGameExist(gameID) {
 
 function getRatings(gameID) {
 	var data = gameData.games[gameID];
-	var total = gameData.length;
-	var result = [{"scoreNorm": data.score / 5.0, "position":(data.rank+"/"+total), "score":""+(Math.round(data.score * 100) / 100), "percentile":(Math.floor((total - data.rank + 1)/total * 1000)/10)+"%"}];
+    var total = gameData.length;
+    var percentile = (data.rank/total) * 100;
+	var result = [{"scoreNorm": data.score / 5.0, "position":(data.rank+"/"+total), "score":""+(Math.round(data.score * 100) / 100), "percentile":"Top " +percentile.toFixed(2)+"%"}];
 	var order = ["Design", "Originality", "Adherence to the Theme"];
 	for (var name of order) {
 		for (var criteria of data.criteria) {
-			if (criteria.name == name) result.push({"scoreNorm": criteria.score / 5.0, "position":(criteria.rank+"/"+total), "score":""+(Math.round(criteria.score * 100) / 100), "percentile":(Math.floor((total - criteria.rank + 1)/total * 1000)/10)+"%"});
+            percentile = (criteria.rank/total) * 100;
+			if (criteria.name == name) result.push({"scoreNorm": criteria.score / 5.0, "position":(criteria.rank+"/"+total), "score":""+(Math.round(criteria.score * 100) / 100), "percentile":"Top " +percentile.toFixed(2)+"%"});
 		}
 	}
 	return result;
